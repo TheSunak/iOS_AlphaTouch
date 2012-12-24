1b5f5ba8b4e6a835f185e3ddae5f1593db226571
Vishal Sunak <vishalsunak@gmail.com>
2012-12-24 16:10:12 -0500

    Initial Commit

---
+++ AlphaTouch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlphaTouch
+//
+//  Created by Vishal Sunak on 12/24/12.
+//  Copyright (c) 2012 Vishal Sunak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AlphaTouch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlphaTouch
+//
+//  Created by Vishal Sunak on 12/24/12.
+//  Copyright (c) 2012 Vishal Sunak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
